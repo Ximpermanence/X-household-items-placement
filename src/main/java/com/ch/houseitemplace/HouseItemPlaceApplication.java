51e272df7d818028fdc5d0555271ab028ce2bd0d
ximpermanence <ximpermanence@qq.com>
2024-12-16 14:52:23 +0800

    1.project init

---
+++ src/main/java/com/ch/houseitemplace/HouseItemPlaceApplication.java
@@ -0,0 +1,13 @@
+package com.ch.houseitemplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseItemPlaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HouseItemPlaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ch/houseitemplace/HouseItemPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ch.houseitemplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseItemPlaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
